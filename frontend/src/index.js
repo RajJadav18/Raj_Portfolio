97991c7b322602f25ccf11ed041886604f3da7c8
Raj Jadav <rajjadav0607@gmail.com>
2026-04-13 19:27:40 +0530

    final flicker fix index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// ✅ Remove flicker (font loading fix)
+window.addEventListener("load", () => {
+  document.body.classList.add("loaded");
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
